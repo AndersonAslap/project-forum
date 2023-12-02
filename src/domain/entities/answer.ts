ff3a2da27467480ddce2b36a56881916cba095bc
AndersonAslap <anderson.adolfo1998@gmail.com>
2023-12-02 13:41:00 -0300

    feat: mapping props

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "../../@shared/entities/entity";
+import { UniqueEntityID } from "../../@shared/entities/unique-entity-id";
 
 type AnswerProps = {
     content: string
-    authorId: string
-    questionId: string
+    authorId: UniqueEntityID
+    questionId: UniqueEntityID
+    createdAt: Date 
+    updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {}
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Slug } from "./value-objects/slug";
 import { Entity } from "../../@shared/entities/entity";
+import { UniqueEntityID } from "../../@shared/entities/unique-entity-id";
 
 type QuestionProps = {
+    authorId: UniqueEntityID
+    bestAnswerId?: UniqueEntityID
     title: string
     content: string
-    authorId: string
     slug: Slug
+    createdAt: Date 
+    updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {}
